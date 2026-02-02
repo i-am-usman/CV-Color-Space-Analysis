ba8559f795ae22850948205bf05bcfd1ad5ed050
i-am-usman <siyalmusman@gmail.com>
2026-02-03 00:34:05 +0500

    RGB Channel Extraction

--- RBG.py
+++ RBG.py
@@ -7,7 +7,6 @@ image = cv2.imread('WhatsApp Image 2026-02-01 at 11.42.52 PM.jpeg')
 # 2. Split into Blue, Green, and Red channels
 # Remember: OpenCV uses BGR order!
 b, g, r = cv2.split(image)
-
 # 3. Create a "Red-only" version (keep Red, set Blue/Green to 0)
 zeros = np.zeros(image.shape[:2], dtype="uint8")
 red_only = cv2.merge([zeros, zeros, r])
