f3175b25ee1903e8b66456bf1b5a9a268066f581
i-am-usman <siyalmusman@gmail.com>
2026-02-04 00:13:18 +0500

    LAB channel extraction

--- CIELAB.py
+++ CIELAB.py
@@ -2,9 +2,10 @@ import cv2
 
 # 1. Load image
 image = cv2.imread('WhatsApp Image 2026-02-01 at 11.42.52 PM.jpeg')
-
+size = (500, 500)
+img = cv2.resize(image, size)
 # 2. Convert to Lab
-lab_image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+lab_image = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
 
 # 3. Split channels
 l, a, b_channel = cv2.split(lab_image)
