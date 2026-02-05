08c83d905c7357a0ec617bcd5ffe06aa0c7fb5d0
Muhammad Usman <siyalmusman@gmail.com>
2026-02-05 10:18:30 +0500

    Updated channel splitting
    Clarified comment to specify splitting channels into l, a, and b.

--- CIELAB.py
+++ CIELAB.py
@@ -7,7 +7,7 @@ img = cv2.resize(image, size)
 # 2. Convert to Lab
 lab_image = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
 
-# 3. Split channels
+# 3. Split channels into l a b
 l, a, b_channel = cv2.split(lab_image)
 
 # 4. Show results
